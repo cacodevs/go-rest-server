10719cc252c3edc0df99cf89cafc85e201ab8fe4
caco <dev@cesar.solutions>
2023-01-03 14:14:18 +0000

    create the first endpoint, first handler and first model

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/cacodevs/go-rest-server
+
+go 1.19
+
+require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.3 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/cacodevs/go-rest-server/handlers"
+	"github.com/cacodevs/go-rest-server/server"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("Error loading .env")
+	}
+
+	PORT := os.Getenv("PORT")
+	JWT_SECRET := os.Getenv("JWT_SECRET")
+	DATABASE_URL := os.Getenv("DATABASE_URL")
+
+	s, err := server.NewServer(context.Background(), &server.Config{
+		JWTSecret:   JWT_SECRET,
+		Port:        PORT,
+		DatabaseUrl: DATABASE_URL,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+    s.Start(BindRoutes)
+}
+
+func BindRoutes(s server.Server, r *mux.Router) {
+	r.HandleFunc("/", handlers.HomeHandler(s)).Methods(http.MethodGet)
+}
