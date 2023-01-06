ef3ae7a7544d017241ee993c6595953d56197dc7
caco <dev@cesar.solutions>
2023-01-06 03:42:01 +0000

    implement postgres to user repository

--- database/postgres.go
+++ database/postgres.go
@@ -1,6 +1,12 @@
 package database
 
-import "database/sql"
+import (
+	"context"
+	"database/sql"
+	"log"
+
+	"github.com/cecardev/go-rest-server/models"
+)
 
 type PostgresRepository struct {
 	db *sql.DB
@@ -12,5 +18,35 @@ func NewPostgresRepository(url string) (*PostgresRepository, error) {
 		return nil, err
 	}
 	return &PostgresRepository{db}, nil
+}
 
+func (repo *PostgresRepository) InsertUser(ctx context.Context, user *models.User) error {
+	_, err := repo.db.ExecContext(ctx, "INSERT INTO users (email, password) VALUES ($1, $2)", user.Email, user.Password)
+	return err
+}
+
+func (repo *PostgresRepository) GetUserById(ctx context.Context, id int64) (*models.User, error) {
+	rows, err := repo.db.QueryContext(ctx, "SELCT id, username, email FROM users WHERE id =$1", id)
+	defer func() {
+		err = rows.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	var user = models.User{}
+	for rows.Next() {
+		if err = rows.Scan(&user.Id, &user.Email); err != nil {
+			return &user, nil
+		}
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+	return &user, err
+}
+
+func (repo *PostgresRepository) Close() error {
+	return repo.Close()
 }
